4f66f9890580508566b60a722deac7877bbbef74
hackwellbn <oburasusan1@gmail.com>
2024-09-14 05:29:07 +0300

    pending updates

--- src/Pages/Home/_Layouts/Hero/Hero.jsx
+++ src/Pages/Home/_Layouts/Hero/Hero.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./Hero.css";
 import { Link } from "react-router-dom";
+
 const Hero = () => {
   return (
     <div className="hero">
-      <h1 className="heading">
-        Elevate Your Projects with Precision Welding Services.
-      </h1>
-      <p className="hero-p">
-        At Elevated Steel Solutions, we pride ourselves on our commitment to
-        quality and customer satisfaction. Our experienced welders use
-        state-of-the-art techniques to ensure your projects are completed to the
-        highest standards.
-      </p>
-<Link>request a quote</Link>
-<Link>Learn more about us</Link>
-
+      <div className="container-hero">
+        <h1 className="heading">
+          Elevate Your Projects with Precision Welding Services.
+        </h1>
+        <p className="hero-p">
+          At Elevated Steel Solutions, we pride ourselves on our commitment to
+          quality and customer satisfaction. Our experienced welders use
+          state-of-the-art techniques to ensure your projects are completed to the
+          highest standards.
+        </p>
+        <div className="link-container">
+          <Link to="/quote" className="navbar-button ">Request a Quote</Link>
+          <Link to="/about" >Learn More About Us</Link>
+        </div>
+      </div>
     </div>
   );
 };
