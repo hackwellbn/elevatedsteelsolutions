8abb56f4979ade3e260ad08c77e160cf63b03a1c
hackwellbn <oburasusan1@gmail.com>
2024-10-02 03:14:42 +0300

    pending updates

--- src/assets/Videos/Videos.js
+++ src/assets/Videos/Videos.js
@@ -2,14 +2,20 @@
 import video1 from "./video1.mp4";
 import video2 from "./video2.mp4";
 import video3 from "./video3.mp4";
+import video4 from "./video4.mp4";
+import video5 from "./video5.mp4";
+import video6 from "./video6.mp4";
+import video7 from "./video7.mp4";
+import video8 from "./video8.mp4";
+
 
 export const videos = [
   { id: 1, src: video1, title: "Video 1" },
   { id: 2, src: video2, title: "Video 2" },
   { id: 3, src: video3, title: "Video 3" },
-  { id: 4, src: "./video4.mp4", title: "Video 4" },
-  { id: 5, src: "./video5.mp4", title: "Video 5" },
-  { id: 6, src: "./video6.mp4", title: "Video 6" },
-  { id: 7, src: "./video7.mp4", title: "Video 7" },
-  { id: 8, src: "./video8.mp4", title: "Video 8" },
+  { id: 4, src: video4, title: "Video 4" },
+  { id: 5, src: video5, title: "Video 5" },
+  { id: 6, src: video6, title: "Video 6" },
+  { id: 7, src: video7, title: "Video 7" },
+  { id: 8, src: video8, title: "Video 8" },
 ];
