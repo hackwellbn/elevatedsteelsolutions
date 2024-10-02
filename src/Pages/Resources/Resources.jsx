8b866f01f1ee2ae39ab7e023d7778c3be780a9fe
hackwellbn <oburasusan1@gmail.com>
2024-10-02 03:33:44 +0300

    pending updates

--- src/Pages/Resources/Resources.jsx
+++ src/Pages/Resources/Resources.jsx
@@ -5,8 +5,11 @@ import VideoPlayer from "./VideoPlayer"; // Import the VideoPlayer component
 import "./Resources.css";
 const Resources = () => {
   return (
-    <div>
+    <div className="gallery">
       <h1>Video Resources</h1>
+      <p>
+        Explore our collection of high-quality, free-to-watch videos to help you understand our products and services better.
+      </p>
       <div className="video-gallery">
         {videos.map((video) => (
           <VideoPlayer key={video.id} src={video.src} title={video.title} />
